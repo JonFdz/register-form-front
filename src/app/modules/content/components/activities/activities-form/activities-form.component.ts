b3fdf1285117f9d3efe2ea6de9b1d94675cee1ee
JonFdz <jonfernal@gmail.com>
2024-08-25 21:05:20 +0200

    Created Activities form component

---
+++ src/app/modules/content/components/activities/activities-form/activities-form.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ActivitiesService } from '@services/activities.service';
+
+@Component({
+	selector: 'app-activities-form',
+	standalone: true,
+	imports: [ReactiveFormsModule, CommonModule],
+	templateUrl: './activities-form.component.html',
+	styleUrl: './activities-form.component.scss'
+})
+export class ActivitiesFormComponent implements OnInit {
+	activitiesForm: FormGroup;
+
+	constructor(
+		private fb: FormBuilder,
+		private activitiesService: ActivitiesService,
+		private router: Router
+	) {
+		this.activitiesForm = this.fb.group({
+			activity_name: ['', Validators.required],
+			instructor: ['', Validators.required],
+			place: [''],
+			day: ['', Validators.required],
+			hour: ['', Validators.required],
+			description: ['']
+		});
+	}
+
+	ngOnInit(): void {
+
+	}
+
+	onSubmit(): void {
+		if (this.activitiesForm.valid) {
+			this.activitiesService.createActivity(this.activitiesForm.value).subscribe((data: any) => {
+				console.log('Actividad creada');
+				this.router.navigate(['/activities']);
+			});
+		} else {
+			console.log('Formulario no válido');
+		}
+	}
+}
