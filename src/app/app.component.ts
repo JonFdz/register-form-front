739ec0bca07ba5b7ed2a1e9f36fbaca5d66eddea
JonFdz <jonfernal@gmail.com>
2024-09-07 22:52:18 +0200

    Created logic for toggle sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './modules/header/header.component';
 import { FooterComponent } from "./modules/footer/footer.component";
+import { SidebarComponent } from './modules/sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'register-form';
+	title = 'register-form';
+
+	sidebarOpen: boolean = false;
+
+	toggleSidebar(): void {
+		this.sidebarOpen = !this.sidebarOpen;
+	}
 }
--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, output } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+	selector: 'app-header',
+	standalone: true,
+	imports: [RouterModule],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+	public onToggleSidebar = output();
 
+	constructor(private authService: AuthService, private router: Router) { }
+
+	isLoggedIn(): boolean {
+		return this.authService.isLoggedIn();
+	}
+
+	handleAuthAction(): void {
+		if (this.isLoggedIn()) {
+			this.authService.logout();
+			this.router.navigate(['/login']);
+		} else {
+			this.router.navigate(['/login']);
+		}
+	}
+
+	toggleSidebar(): void {
+		if (this.isLoggedIn()) {
+			this.onToggleSidebar.emit();
+		} else {
+			this.router.navigate(['/login']);
+		}
+	}
 }
