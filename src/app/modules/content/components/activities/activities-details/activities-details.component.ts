9ce3833370988c1028c4c318a8dd9157f736ea64
JonFdz <jonfernal@gmail.com>
2024-09-08 18:05:07 +0200

    Created activities details component

---
+++ src/app/modules/content/components/activities/activities-details/activities-details.component.ts
@@ -0,0 +1,50 @@
+import { Component, input, OnInit, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ActivitiesService } from '@services/activities.service';
+import { Activity } from '@models/activity.model';
+import { Router } from '@angular/router';
+
+
+@Component({
+	selector: 'app-activities-details',
+	templateUrl: './activities-details.component.html',
+	styleUrls: ['./activities-details.component.scss'],
+	standalone: true,
+	imports: [CommonModule]
+})
+export class ActivitiesDetailsComponent implements OnInit {
+	activity: Activity = {} as Activity;
+	activityId = input.required<number>();
+	closeDialog = output<void>();
+
+	constructor(
+		private usersService: ActivitiesService,
+		private router: Router
+	) { }
+
+	ngOnInit(): void {
+		if (this.activityId()) {
+			this.usersService.getActivity(this.activityId()).subscribe(data => {
+				this.activity = data;
+			});
+		}
+	}
+
+	onCloseDialog(): void {
+		this.closeDialog.emit();
+	}
+
+	deleteActivity(): void {
+		if (confirm(`¿Estás seguro de que quieres eliminar el intercambio ${this.activity.activity_name}?`)) {
+			this.usersService.deleteActivity(this.activityId()!).subscribe(() => {
+				this.closeDialog.emit();
+			});
+		};
+	}
+
+	updateActivity(): void {
+		this.router.navigate(['/activities/register', this.activity.activity_id]);
+		this.closeDialog.emit();
+	}
+}
