1e38b8c006f0741cbca70a13ff54910182e425d3
JonFdz <jonfernal@gmail.com>
2024-09-15 11:49:23 +0200

    Updated dialog component to work with confirm-message

--- src/app/modules/content/components/shared/dialog/dialog.component.ts
+++ src/app/modules/content/components/shared/dialog/dialog.component.ts
@@ -4,17 +4,19 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UsersDetailsComponent } from '@users/users-details/users-details.component';
 import { ActivitiesDetailsComponent } from '@activities/activities-details/activities-details.component';
 import { StatusMessageComponent } from '@sharedcontent/status-message/status-message.component';
+import { ConfirmMessageComponent } from '@sharedcontent/confirm-message/confirm-message.component';
 
 export enum DialogType {
 	Users = 'Users',
 	Activities = 'Activities',
-	Status = 'Status'
+	Status = 'Status',
+	Confirm = 'Confirm'
 }
 
 @Component({
 	selector: 'app-dialog',
 	standalone: true,
-	imports: [ UsersDetailsComponent, ActivitiesDetailsComponent, StatusMessageComponent],
+	imports: [UsersDetailsComponent, ActivitiesDetailsComponent, StatusMessageComponent, ConfirmMessageComponent],
 	templateUrl: './dialog.component.html',
 	styleUrl: './dialog.component.scss'
 })
@@ -34,8 +36,11 @@ export class DialogComponent implements OnInit {
 		this.message = this.data.message ? this.data.message : '';
 	}
 
-	closeDialog(): void {
-		this.dialogRef.close();
+	closeDialog(data?: any): void {
+		if (data) {
+			this.dialogRef.close(data);
+		} else {
+			this.dialogRef.close();
+		}
 	}
-
 }
