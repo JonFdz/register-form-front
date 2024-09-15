6da1d8a90955b66dcc4ccaaade9f5706d63bce90
JonFdz <jonfernal@gmail.com>
2024-09-15 19:51:08 +0200

    Added vercel config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+	production: true,
+	apiUrlActivities: process.env.API_URL_ACTIVITIES,
+	apiUrlInscriptions: process.env.API_URL_INSCRIPTIONS,
+	apiUrlUsers: process.env.API_URL_USERS,
+	apiUrlAuth: process.env.API_URL_AUTH
+  };
