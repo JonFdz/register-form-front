4476bb02375068b8fab614ca558d56a5141c8f91
JonFdz <jonfernal@gmail.com>
2024-09-15 10:38:19 +0200

    Updated inscription model with new fields and without FK

--- src/app/modules/content/models/inscription.model.ts
+++ src/app/modules/content/models/inscription.model.ts
@@ -3,13 +3,29 @@ import { Activity } from './activity.model';
 
 export interface Inscription {
 	inscription_id?: number;
-	user_id: User['user_id'];
-	activity1_id: Activity['activity_id'];
-	activity2_id?: Activity['activity_id'];
-	activity3_id?: Activity['activity_id'];
-	activity4_id?: Activity['activity_id'];
-	activity5_id?: Activity['activity_id'];
-	fee: number;
-	referred: string;
 	created_at?: string;
+	user_id: User['user_id'];
+	user_name: User['user_name'];
+	last_name: User['last_name'];
+	gender: User['gender'];
+	birthdate: User['birthdate'];
+	birthplace: User['birthplace'];
+	phone: User['phone'];
+	email?: User['email'];
+	activity1_name: Activity['activity_name'];
+	activity1_instructor: Activity['instructor'];
+	activity1_day: Activity['day'];
+	activity1_hour: Activity['hour'];
+	activity2_name?: Activity['activity_name'];
+	activity2_instructor?: Activity['instructor'];
+	activity2_day?: Activity['day'];
+	activity2_hour?: Activity['hour'];
+	activity3_name?: Activity['activity_name'];
+	activity3_instructor?: Activity['instructor'];
+	activity3_day?: Activity['day'];
+	activity3_hour?: Activity['hour'];
+	abilities?: User['abilities'];
+	fee: number;
+	activities_selected: number;
+	referred: string;
 }
